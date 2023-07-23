a490f58e10ad8e2998eaec01193307cb7e4b20b8
s74Bits <92068736+s74Bits@users.noreply.github.com>
2023-07-23 17:10:10 -0500

    asdfdsafsadfsdaf

--- UnityWebglHtmlGenerator.js
+++ UnityWebglHtmlGenerator.js
@@ -1,2 +1,20 @@
+var path = "Builds/Experimental/";
+document.write(`<link rel="shortcut icon" href="${path}TemplateData/favicon.ico">`);
+document.write(`<link rel="stylesheet" href="${path}TemplateData/style.css">`);
+
+document.write(`<div id="unity-container" class="unity-desktop">
+    <canvas id="unity-canvas" width="100%" height="100%"></canvas>
+    <div id="unity-loading-bar" style="display: none;">
+        <div id="unity-logo"></div>
+        <div id="unity-progress-bar-empty">
+            <div id="unity-progress-bar-full" style="width: 100%;"></div>
+        </div>
+    </div>
+    <div id="unity-warning"></div>
+    <div id="unity-footer">
+        <div id="unity-webgl-logo"></div>
+        <div id="unity-fullscreen-button"></div>
+        <div id="unity-build-title">Five Nights At Bari's</div>
+    </div>`);
 document.write("<p>hello</p>");
 document.write("<p>a</p>");
